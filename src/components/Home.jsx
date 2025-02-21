92f1ebd52181e7b1d66bfed7a0fbd1fac121da83
yaswanth <yaswanthkonathala777@gmail.com>
2025-02-21 18:33:54 +0530

    post width fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,8 +27,8 @@ const Home = ({ loggedin, setLoggedin, create }) => {
         {loggedin && (
           <div className="w-full flex justify-center bg-gray-900 top-0 sticky">
             {/* narrow width and expand on smaller screens */}
-            <div className="lg:w-1/2 md:w-1/2 xs:w-full w-full ">
-              <div className=" w-fullp-10 rounded-md">
+            <div className="md:w-2/3 xs:w-full w-full ">
+              <div className=" w-full p-10 rounded-md">
                 {!createpost ? (
                   <PostsList />
                 ) : (
